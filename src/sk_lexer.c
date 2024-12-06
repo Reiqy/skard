dbe94a0c521cc5a0b21e5076f25918a917f010d1
Reiqy <45209567+Reiqy@users.noreply.github.com>
2024-12-07 00:31:55 +0100

    Reorganize lexer and implement keyword lexing

--- src/sk_lexer.c
+++ src/sk_lexer.c
@@ -3,30 +3,36 @@
 #include <stdbool.h>
 #include <string.h>
 
-static struct sk_token make_token(struct sk_lexer *lexer, enum sk_token_type type);
-static struct sk_token make_token_eof(struct sk_lexer *lexer);
-static struct sk_token make_token_err(struct sk_lexer *lexer, const char *message);
-
-static struct sk_token scan_number(struct sk_lexer *lexer);
-
-static bool is_at_eof(struct sk_lexer *lexer);
-
-static char advance(struct sk_lexer *lexer);
-static char peek(struct sk_lexer *lexer);
-static char peek_next(struct sk_lexer *lexer);
-
-static bool is_alpha(char c);
-static bool is_digit(char c);
-
-static bool is_whitespace(char c);
-static void skip_whitespace(struct sk_lexer *lexer);
-
 void sk_lexer_init(struct sk_lexer *lexer, const char *source)
 {
     lexer->start = source;
     lexer->current = source;
     lexer->line = 1;
 }
+
+static size_t get_token_length(const struct sk_lexer *lexer);
+
+static struct sk_token make_token(const struct sk_lexer *lexer, enum sk_token_type type);
+static struct sk_token make_token_eof(const struct sk_lexer *lexer);
+static struct sk_token make_token_err(const struct sk_lexer *lexer, const char *message);
+
+static bool is_at_eof(const struct sk_lexer *lexer);
+
+static bool is_alpha(char c);
+static bool is_digit(char c);
+static bool is_whitespace(char c);
+
+static char advance(struct sk_lexer *lexer);
+static char peek(const struct sk_lexer *lexer);
+static char peek_next(const struct sk_lexer *lexer);
+
+static void skip_whitespace(struct sk_lexer *lexer);
+
+enum sk_token_type check_keyword(const struct sk_lexer *lexer, int offset, const char *keyword, enum sk_token_type type);
+enum sk_token_type classify_identifier(const struct sk_lexer *lexer);
+
+static struct sk_token scan_number(struct sk_lexer *lexer);
+static struct sk_token scan_identifier(struct sk_lexer *lexer);
 
 struct sk_token sk_lexer_next(struct sk_lexer *lexer)
 {
@@ -40,7 +46,7 @@ struct sk_token sk_lexer_next(struct sk_lexer *lexer)
     char c = advance(lexer);
 
     if (is_alpha(c)) {
-        return make_token_err(lexer, "Not implemented yet.");
+        return scan_identifier(lexer);
     }
 
     if (is_digit(c)) {
@@ -48,9 +54,6 @@ struct sk_token sk_lexer_next(struct sk_lexer *lexer)
     }
 
     switch (c) {
-        case '#':
-            // TODO: This is temporary to test statement parsing.
-            return make_token(lexer, SK_TOKEN_PRINT);
         case '(':
             return make_token(lexer, SK_TOKEN_LPAREN);
         case ')':
@@ -70,24 +73,29 @@ struct sk_token sk_lexer_next(struct sk_lexer *lexer)
     return make_token_err(lexer, "Unexpected character.");
 }
 
-static struct sk_token make_token(struct sk_lexer *lexer, enum sk_token_type type)
+static size_t get_token_length(const struct sk_lexer *lexer)
+{
+    return lexer->current - lexer->start;
+}
+
+static struct sk_token make_token(const struct sk_lexer *lexer, enum sk_token_type type)
 {
     struct sk_token token = {
         .type = type,
         .start = lexer->start,
-        .length = lexer->current - lexer->start,
+        .length = get_token_length(lexer),
         .line = lexer->line,
     };
 
     return token;
 }
 
-static struct sk_token make_token_eof(struct sk_lexer *lexer)
+static struct sk_token make_token_eof(const struct sk_lexer *lexer)
 {
     return make_token(lexer, SK_TOKEN_EOF);
 }
 
-static struct sk_token make_token_err(struct sk_lexer *lexer, const char *message)
+static struct sk_token make_token_err(const struct sk_lexer *lexer, const char *message)
 {
     struct sk_token token = {
         .type = SK_TOKEN_ERR,
@@ -97,6 +105,87 @@ static struct sk_token make_token_err(struct sk_lexer *lexer, const char *messag
     };
 
     return token;
+}
+
+static bool is_at_eof(const struct sk_lexer *lexer)
+{
+    return peek(lexer) == '\0';
+}
+
+static bool is_alpha(char c)
+{
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+}
+
+static bool is_digit(char c)
+{
+    return c >= '0' && c <= '9';
+}
+
+static bool is_whitespace(char c)
+{
+    return c == ' ' || c == '\t' || c == '\n' || c == '\r';
+}
+
+static char advance(struct sk_lexer *lexer)
+{
+    char c = peek(lexer);
+    if (c == '\n') {
+        lexer->line++;
+    }
+
+    lexer->current++;
+    return c;
+}
+
+static char peek(const struct sk_lexer *lexer)
+{
+    return *lexer->current;
+}
+
+static char peek_next(const struct sk_lexer *lexer)
+{
+    if (is_at_eof(lexer)) {
+        return '\0';
+    }
+
+    return lexer->current[1];
+}
+
+static void skip_whitespace(struct sk_lexer *lexer)
+{
+    for (;;) {
+        char c = peek(lexer);
+        if (!is_whitespace(c)) {
+            break;
+        }
+
+        advance(lexer);
+    }
+}
+
+enum sk_token_type check_keyword(const struct sk_lexer *lexer, int offset, const char *keyword, enum sk_token_type type)
+{
+    size_t keyword_length = strlen(keyword);
+    size_t token_length = get_token_length(lexer);
+    bool is_length_same = token_length == keyword_length;
+    if (is_length_same && memcmp(lexer->start + offset, keyword + offset, keyword_length - offset) == 0) {
+        return type;
+    }
+
+    return SK_TOKEN_IDENTIFIER;
+}
+
+enum sk_token_type classify_identifier(const struct sk_lexer *lexer)
+{
+    switch (lexer->start[0]) {
+        case 'p':
+            return check_keyword(lexer, 1, "print", SK_TOKEN_PRINT);
+        default:
+            break;
+    }
+
+    return SK_TOKEN_IDENTIFIER;
 }
 
 static struct sk_token scan_number(struct sk_lexer *lexer)
@@ -116,59 +205,11 @@ static struct sk_token scan_number(struct sk_lexer *lexer)
     return make_token(lexer, SK_TOKEN_NUMBER);
 }
 
-static bool is_at_eof(struct sk_lexer *lexer)
+static struct sk_token scan_identifier(struct sk_lexer *lexer)
 {
-    return peek(lexer) == '\0';
-}
-
-static char advance(struct sk_lexer *lexer)
-{
-    char c = peek(lexer);
-    if (c == '\n') {
-        lexer->line++;
-    }
-
-    lexer->current++;
-    return c;
-}
-
-static char peek(struct sk_lexer *lexer)
-{
-    return *lexer->current;
-}
-
-static char peek_next(struct sk_lexer *lexer)
-{
-    if (is_at_eof(lexer)) {
-        return '\0';
-    }
-
-    return lexer->current[1];
-}
-
-static bool is_alpha(char c)
-{
-    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
-}
-
-static bool is_digit(char c)
-{
-    return c >= '0' && c <= '9';
-}
-
-static bool is_whitespace(char c)
-{
-    return c == ' ' || c == '\t' || c == '\n' || c == '\r';
-}
-
-static void skip_whitespace(struct sk_lexer *lexer)
-{
-    for (;;) {
-        char c = peek(lexer);
-        if (!is_whitespace(c)) {
-            break;
-        }
-
+    while (is_alpha(peek(lexer)) || is_digit(peek(lexer))) {
         advance(lexer);
     }
+
+    return make_token(lexer, classify_identifier(lexer));
 }
